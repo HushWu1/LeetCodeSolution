cbaf436ac36a956db3bb7531e87628fb2fa6e067
HushWu1 <wpy2316501997@163.com>
2023-09-12 15:19:57 +0800

    第一天算法 合并链表排序

---
+++ 合并链表排序/合并链表排序/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 合并链表排序
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ListNode n1 = new ListNode(1);
+            n1.next = new ListNode(2);
+            n1.next.next = new ListNode(3);
+            Console.WriteLine("n1链表：\t");
+            Solution.showNode(n1);
+
+            ListNode n2 = new ListNode(1);
+            n2.next = new ListNode(3);
+            n2.next.next = new ListNode(4);
+            Console.WriteLine("n2链表：\t");
+            Solution.showNode(n2);
+
+            //调用方法进行合并
+            ListNode n3 = Solution.MergeTwoLists(n1, n2);
+            //排序完成后输出
+            Console.WriteLine("排序后输出结果" + "\t");
+            Solution.showNode(n3);
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
+
---
+++ 合并链表排序/合并链表排序/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 合并链表排序
+{
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+
+        public static void showNode(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.val + " ");
+                node = node.next;
+            }
+        }
+
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode temp = new ListNode(0);
+            ListNode l3 = temp;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    temp.next = l1;
+                    temp = temp.next;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    temp.next = l2;
+                    temp = temp.next;
+                    l2 = l2.next;
+                }
+            }
+            if (l1 == null)
+                temp.next = l2;
+            else if (l2 == null)
+                temp.next = l1;
+            return l3.next;
+        }
+
+    }
+}
